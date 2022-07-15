e9df4232bb66a1a5d0f90fa6019df07a224c5ed0
ergo.dvoretskiy <ergo.dvoretskiy@gmail.com>
2022-07-15 16:10:36 +0300

    Make gui skeleton.

--- FJournal/FJournalGUI/MainWindow.xaml.cs
+++ FJournal/FJournalGUI/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using FJournalLib;
+﻿using FJournalGUI.ViewModels;
+using FJournalLib;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -22,32 +23,23 @@ namespace FJournalGUI
     /// </summary>
     public partial class MainWindow : Window
     {
-        private readonly Journal _journal;
-
-        private readonly Timer _writeToMongoTimer;
-
-        private readonly int _mongoTimerInterval = 1;
-
         public MainWindow()
         {
             InitializeComponent();
 
-            this._journal = new FJournalLib.Journal();
-
-            this._writeToMongoTimer = new Timer();
-            this._writeToMongoTimer.Elapsed += _writeToMongoTimer_Elapsed;
-            this._writeToMongoTimer.Interval = this._mongoTimerInterval;
-
-            this._writeToMongoTimer.Start();
+            this.DataContext = new ApplicationViewModel();
         }
 
-        private void _writeToMongoTimer_Elapsed(object? sender, ElapsedEventArgs e)
+        private void dg_dbRecords_MouseDoubleClick(object sender, MouseButtonEventArgs e)
         {
-            this._journal.Note(new FJournalLib.Models.TRecord()
-            {
-                Message = $"Hello from FJournal GUI, {Guid.NewGuid()}",
-                LogType = FJournalLib.Enums.LogType.Info
-            });
+            if (dg_dbRecords.SelectedItem == null)
+                return;
+
+            var selectedRow = dg_dbRecords.SelectedItem as DBRecordViewModel;
+
+            if (selectedRow != null)
+                MessageBox.Show($"You've picked the wrong house, fool. Message: {selectedRow.Message}");
+
         }
     }
 }
